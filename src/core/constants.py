9acf9b65496cbb2a074c1a870bd0668715bbd276
ako.nibladze <ako.nibladze@gmail.com>
2024-03-16 23:18:43 +0400

    add: appending in database

--- src/core/constants.py
+++ src/core/constants.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = "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"
+API_KEY = ""
 MODEL_NAME = "gpt-3.5-turbo-0125"
 TOKEN_LIMIT = 4096
 VERBOSE = False
